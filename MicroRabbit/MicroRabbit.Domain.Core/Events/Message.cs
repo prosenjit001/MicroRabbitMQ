91ae0fb6e5677656c5615a1fb693da1cc3bf685d
Prosenjit Mitra <mitra.prosenjit2008@gmail.com>
2021-02-12 17:50:09 +0530

    Commit

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/Message.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Message : IRequest<bool>
+    {
+
+    }
+}
